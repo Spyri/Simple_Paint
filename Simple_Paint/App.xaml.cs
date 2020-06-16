295d634393063fd9c09bd5f3fd75be881bf01a18
Spyridon Spyropoulos <50782566+Spyri@users.noreply.github.com>
2020-06-16 20:29:32 +0200

    Created Window

---
+++ Simple_Paint/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_Paint
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Simple_Paint/ViewModel/SimplePaintViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Simple_Paint.Annotations;
+
+namespace Simple_Paint.ViewModel
+{
+    public class SimplePaintViewModel : INotifyPropertyChanged
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
